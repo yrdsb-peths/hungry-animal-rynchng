af0bfea1b3af3e577535bee7da7aa328b4c6522f
rynchng <150175907+rynchng@users.noreply.github.com>
2023-11-23 12:33:43 -0500

    part 1

---
+++ Elephant.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The elephant
+ * 
+ * @author ryan chang
+ * @version November 2023
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephantt wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("left"))
+        {
+            move(-2);
+        }
+        else if (Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+        }
+        
+        // remove apple if elephant touches it
+        eat();
+    }
+    
+    public void eat()
+    {
+        if (isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
- * Write a description of class MyWorld here.
+ * The coolest world
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author ryan chang
+ * @version November 2023
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +16,37 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false); 
+        
+        // Create elephant object
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        // Create label
+        scoreLabel = new Label(0, 75);
+        addObject(scoreLabel, 50, 50);
+        
+        
+        createApple();
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+    }
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over" , 100);
+        addObject(gameOverLabel, 300, 200);
     }
 }
